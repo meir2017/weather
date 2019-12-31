0935b3f86ae3bb4e9f1326d397cad790900a4f09
meir2017 <66meir46@gmail.com>
2019-12-31 21:41:40 +0200

    Updates

---
+++ precache-manifest.62acbe4698d0be4dad5ca6031df166ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c30b2595325e1c65211455653f3ca8",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "3a8646099352c3b0b565",
+    "url": "/weather/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f15d47ce757209e0e4c4",
+    "url": "/weather/static/css/main.acef12af.chunk.css"
+  },
+  {
+    "revision": "3a8646099352c3b0b565",
+    "url": "/weather/static/js/2.9aa655a9.chunk.js"
+  },
+  {
+    "revision": "75d3642ff4c941fd0dd57f8aeca9426d",
+    "url": "/weather/static/js/2.9aa655a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f15d47ce757209e0e4c4",
+    "url": "/weather/static/js/main.b9e93931.chunk.js"
+  },
+  {
+    "revision": "6ea7e77960c1989f63b6",
+    "url": "/weather/static/js/runtime-main.f857f3f3.js"
+  }
+]);
